afee806c7a7aecb4d882f71a2c3978c9c2c89002
Ali <ali512@outlook.com>
2020-08-06 13:34:16 +0300

    Add project files.

---
+++ EFEnhancer/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿using ActiproSoftware.SyntaxEditor;
+
+namespace EFEnhancer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            ActiproSoftware.SyntaxEditor.Document document1 = new ActiproSoftware.SyntaxEditor.Document();
+            ActiproSoftware.SyntaxEditor.Document document2 = new ActiproSoftware.SyntaxEditor.Document();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.syntaxEditor1 = new ActiproSoftware.SyntaxEditor.SyntaxEditor();
+            this.cSharpSyntaxLanguage1 = new ActiproSoftware.SyntaxEditor.Addons.CSharp.CSharpSyntaxLanguage(this.components);
+            this.syntaxEditor2 = new ActiproSoftware.SyntaxEditor.SyntaxEditor();
+            this.xmlSyntaxLanguage1 = new ActiproSoftware.SyntaxEditor.Addons.Xml.XmlSyntaxLanguage(this.components);
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(243, 836);
+            this.treeView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // syntaxEditor1
+            // 
+            this.syntaxEditor1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.syntaxEditor1.Document = document1;
+            this.syntaxEditor1.Location = new System.Drawing.Point(0, 0);
+            this.syntaxEditor1.Name = "syntaxEditor1";
+            this.syntaxEditor1.Size = new System.Drawing.Size(423, 836);
+            this.syntaxEditor1.SplitType = ActiproSoftware.SyntaxEditor.SyntaxEditorSplitType.None;
+            this.syntaxEditor1.TabIndex = 3;
+            this.syntaxEditor1.ViewVerticalScroll += new ActiproSoftware.SyntaxEditor.EditorViewEventHandler(this.syntaxEditor1_ViewVerticalScroll);
+            // 
+            // syntaxEditor2
+            // 
+            this.syntaxEditor2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.syntaxEditor2.Document = document2;
+            this.syntaxEditor2.Location = new System.Drawing.Point(0, 0);
+            this.syntaxEditor2.Name = "syntaxEditor2";
+            this.syntaxEditor2.Size = new System.Drawing.Size(377, 836);
+            this.syntaxEditor2.SplitType = ActiproSoftware.SyntaxEditor.SyntaxEditorSplitType.None;
+            this.syntaxEditor2.TabIndex = 4;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1051, 836);
+            this.splitContainer1.SplitterDistance = 243;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.syntaxEditor1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.syntaxEditor2);
+            this.splitContainer2.Size = new System.Drawing.Size(804, 836);
+            this.splitContainer2.SplitterDistance = 423;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.splitContainer1);
+            this.splitContainer3.Size = new System.Drawing.Size(1051, 886);
+            this.splitContainer3.SplitterDistance = 46;
+            this.splitContainer3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 886);
+            this.Controls.Add(this.splitContainer3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private SyntaxEditor syntaxEditor1;
+        private ActiproSoftware.SyntaxEditor.Addons.CSharp.CSharpSyntaxLanguage cSharpSyntaxLanguage1;
+        private SyntaxEditor syntaxEditor2;
+        private ActiproSoftware.SyntaxEditor.Addons.Xml.XmlSyntaxLanguage xmlSyntaxLanguage1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+    }
+}
+
---
+++ EFEnhancer/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFEnhancer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            treeView1.AfterSelect += treeView1_AfterSelect;
+
+            var path = @"C:\Users\Ali\Source\Repos\WorkflowWeb\WorkflowWeb";
+            BuildTree(new DirectoryInfo(path), treeView1.Nodes);
+            //remove unnecessary folders
+            string[] valid = "Areas,Controllers,Views".Split(',');
+            List<TreeNode> toRemove = new List<TreeNode>();
+            foreach(TreeNode n in treeView1.Nodes[0].Nodes)
+            {
+                if(!valid.Contains(n.Text))
+                {
+                    toRemove.Add(n);                    
+                }
+            }
+
+            toRemove.ForEach((n) => { treeView1.Nodes.Remove(n); });
+
+            treeView1.ExpandAll();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    var di = new DirectoryInfo(fbd.SelectedPath);
+                    BuildTree(di, treeView1.Nodes);
+                }
+            }
+        }
+
+        private void BuildTree(DirectoryInfo directoryInfo, TreeNodeCollection addInMe)
+        {
+            TreeNode curNode = addInMe.Add(directoryInfo.Name);
+
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                curNode.Nodes.Add(file.FullName, file.Name);
+            }
+            foreach (DirectoryInfo subdir in directoryInfo.GetDirectories())
+            {
+                BuildTree(subdir, curNode.Nodes);
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+            if (e.Node.Name.EndsWith(".cs") || e.Node.Name.EndsWith(".cshtml"))
+            {
+               
+                StreamReader reader = new StreamReader(e.Node.Name);
+                if (e.Node.Name.EndsWith(".cs"))
+                {
+                    syntaxEditor1.Document.Language = cSharpSyntaxLanguage1;
+                    syntaxEditor2.Document.Language = syntaxEditor1.Document.Language;
+                }
+                else
+                {
+                    syntaxEditor1.Document.Language = xmlSyntaxLanguage1;
+                    syntaxEditor2.Document.Language = syntaxEditor1.Document.Language;
+                }
+
+
+                syntaxEditor1.Text = reader.ReadToEnd();
+                
+                syntaxEditor2.Text = syntaxEditor1.Text;
+                reader.Close();
+            }
+        }
+
+        private void syntaxEditor1_ViewVerticalScroll(object sender, ActiproSoftware.SyntaxEditor.EditorViewEventArgs e)
+        {
+            //MessageBox.Show(e.View.FirstVisibleDisplayLineIndex.ToString());
+            var line = e.View.FirstVisibleDisplayLineIndex;
+            syntaxEditor2.Views[0].FirstVisibleDisplayLineIndex = line;
+        }
+    }
+}
